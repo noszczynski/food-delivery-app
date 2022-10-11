6a28593c3fd9b1ca9f164d73e82a4866cb081ea4
adamnoszczynski <adam.noszczynski@gmail.com>
2022-10-11 18:43:56 +0200

    update fake-data.js

--- backend-knex/knexfile.js
+++ backend-knex/knexfile.js
@@ -5,47 +5,53 @@
  */
 export default {
 
-  development: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-      filename: './src/database/knex-dev-base.sqlite3'
-    },
-    migrations: {
-      directory: "./src/database/migrations"
-    }
-  },
+	development: {
+		client: "sqlite3",
+		useNullAsDefault: true,
+		connection: {
+			filename: "./src/database/knex-dev-base.sqlite3"
+		},
+		migrations: {
+			directory: "./src/database/migrations"
+		},
+		seeds: {
+			directory: "./src/database/seeds"
+		},
+		pool: {
+			afterCreate: (knex, cb) => knex. run("PRAGMA foreign_keys = ON", cb)
+		}
+	},
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
+	staging: {
+		client: "postgresql",
+		connection: {
+			database: "my_db",
+			user:     "username",
+			password: "password"
+		},
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			tableName: "knex_migrations"
+		}
+	},
 
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+	production: {
+		client: "postgresql",
+		connection: {
+			database: "my_db",
+			user:     "username",
+			password: "password"
+		},
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			tableName: "knex_migrations"
+		}
+	}
 
 };
