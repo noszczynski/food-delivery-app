faf43f4969dc29fa8f321b5157d7c6f41a2fc24e
adamnoszczynski <adam.noszczynski@gmail.com>
2022-10-30 10:32:36 +0100

    prisma seed and finish

---
+++ backend-prisma/test-queries/restaurants.query.js
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+const restaurants = await prisma.restaurant.findMany({});
+
+await prisma.$disconnect();
+
+console.log(restaurants);
+ 
